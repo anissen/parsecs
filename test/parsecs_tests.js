0c7607c3eec51a33717ef18f706715d23a05f924
Anders Nissen <andnis@gmail.com>
2014-02-03 23:02:40 +0100

    Added initial test file

---
+++ test/parsecs_tests.js
@@ -0,0 +1,26 @@
+
+var assert = require('chai').assert;
+
+/*
+var Parsecs = require('./../src/parsecs');
+var parsecs = new Parsecs();
+
+describe('parsecs', function() {
+  it ('should be a function', function() {
+    assert.equal(typeof(parsecs), "function");
+  });
+});
+*/
+
+var systems = require('./../src/systems/systems');
+
+['RenderSystem', 'MotionSystem', 'TraceSystem'].forEach(function(system) {
+  describe(system, function() {
+    it ('should be an object', function() {
+      assert.isObject(systems[system]);
+    });
+    it ('should contain a "tick" function', function() {
+      assert.isFunction(systems[system].tick);
+    });
+  });
+});
