e49a783c210e582458aa8b5f8f41d441b7c76554
Anders Nissen <andnis@gmail.com>
2014-02-04 09:34:42 +0100

    Remove gulp-notify to fix travis

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ gulp.task('build', function() {
   var rename = require("gulp-rename");
   var uglify = require("gulp-uglify");
   var license = require('gulp-license');
-  var notify = require("gulp-notify");
+  //var notify = require("gulp-notify");
   
   gulp.src(scripts.main) // sources
     .pipe(browserify({
@@ -77,8 +77,8 @@ gulp.task('build', function() {
     .pipe(bytediff.stop())
     .pipe(header('/* This is a header for minified  ${name} version ${version}! */\n', { name: 'gulp test', version: '0.0.2' } ))
     .pipe(license('MIT', { tiny: true, organization: 'Anders Nissen' }))
-    .pipe(gulp.dest(dist + 'bundle'))
-    .pipe(notify({ message: 'build task completed' }));
+    .pipe(gulp.dest(dist + 'bundle'));
+    //.pipe(notify({ message: 'build task completed' }));
 });
 
 gulp.task('watch', function() {
